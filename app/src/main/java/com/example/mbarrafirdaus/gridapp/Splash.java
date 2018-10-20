5c57acb3ac914bdb89a27a4bc0ddf83f51a891b2
muhF1508 <firdausbarra08@gmail.com>
2018-10-20 13:17:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mbarrafirdaus/gridapp/Splash.java
@@ -0,0 +1,25 @@
+package com.example.mbarrafirdaus.gridapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, AndroidGridLayoutActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000L);
+    }
+}
